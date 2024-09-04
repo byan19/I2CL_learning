525c7f912848becce73615704154e64c4fc4f097
byan19 <12835163+aisdante@user.noreply.gitee.com>
2024-09-04 11:34:32 +0100

    boyan_mac

--- configs/config_i2cl.py
+++ configs/config_i2cl.py
@@ -5,7 +5,7 @@ import my_datasets as md
 
 config = {}
 # general
-config['exp_name'] = 'exps/i2cl_debug'
+config['exp_name'] = 'exps/i2cl_2batchsize'
 config['gpus'] = ['0']
 config['models'] = ['meta-llama/Llama-2-7b-hf']  # 'gpt2-xl', 'EleutherAI/gpt-j-6B'
 #config['models'] = ['gpt2-xl']  # 'gpt2-xl', 'EleutherAI/gpt-j-6B'
