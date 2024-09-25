b5366677bae382da89ed26ca288fd3e44bdd138f
byan19 <12835163+aisdante@user.noreply.gitee.com>
2024-09-25 16:52:52 +0100

    boyan_mac

--- configs/config_layernorm_adaption.py
+++ configs/config_layernorm_adaption.py
@@ -7,7 +7,7 @@ config = {}
 # general
 config['exp_name'] = 'exps/baseline_layernorm_Llama2_debug'
 config['gpus'] = ['0']
-config['models'] = ['meta-llama/Llama-2-7b-hf'] # 'meta-llama/Meta-Llama-3-8B', 'gpt2-xl', 'meta-llama/Llama-2-7b-hf', 'EleutherAI/gpt-j-6B'
+config['models'] = ['meta-llama/Meta-Llama-3-8B'] # 'meta-llama/Meta-Llama-3-8B', 'gpt2-xl', 'meta-llama/Llama-2-7b-hf', 'EleutherAI/gpt-j-6B'
 config['datasets'] = list(md.target_datasets.keys())
 config['seed'] = 42
 config['run_num'] = 5
