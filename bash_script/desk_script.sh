c0923fe3766ae5a70fb17ef347180083c2e0e18f
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-03-14 14:29:11 +0000

    boyan_mac

---
+++ bash_script/desk_script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+MultiRun (){
+sessionname=$1
+File=$2
+Param=$3
+Tunning=$4
+echo "${sessionname}"
+
+screen -dmS "$sessionname"
+
+screen -S $sessionname -X stuff "cd /home/engs2575/project/I2CL_learning^M"
+screen -S $sessionname -X stuff "source activate lm_eval^M"
+sleep 1.0s
+echo ${File}
+screen -S $sessionname -X stuff "bash ${File} ${Param} ${sessionname} ${Tunning}^M"
+}
+
+Tunning=1
+
+Target_Para_List=(o_proj q_proj k_proj v_proj)
+Target_Para_List=(o_proj)
+Tunning_list=(0 )
+for ((idx=0; idx<${#Target_Para_List[@]}; idx++)); do
+Param=${Target_Para_List[$idx]}
+Tunning=${Tunning_list[$idx]}
+
+sessionname=inputlayernorm_sgd
+File=bash_script/bash_sub_script/sub_layernorm_adaptation.sh
+
+CHECK=${sessionname}
+
+MultiRun ${sessionname} ${File} ${Param} ${Tunning}
+done
