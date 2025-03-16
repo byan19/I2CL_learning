00d7d8e96765f3e8c39455e286a7e80a650e09c4
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-03-16 20:31:56 +0000

    boyan_mac

--- self_exploration_tool.py
+++ self_exploration_tool.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import pdb
 class RescaledLayerNormPEFT(nn.Module):
 	def __init__(self, original_ln: nn.LayerNorm, alpha=1.0, trainable_alpha=False, mode="add"):
 		super().__init__()
@@ -56,6 +57,7 @@ def patch_layernorm_with_rescaled_by_name(model, alpha=1.0, mode="add", match_ke
 	"""
 	for name, module in model.named_modules():
 		#if isinstance(module, nn.LayerNorm) and any(k in name.lower() for k in match_keywords):
+		pdb.set_trace()
 		if isinstance(module, nn.LayerNorm) and match_key in name :
 		# Identify the parent module
 			print('trigger')
