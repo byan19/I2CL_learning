9f472c7b2942f39ac794b63b072a0ff9f74ea148
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-03-16 21:30:06 +0000

    boyan_mac

--- self_exploration_tool.py
+++ self_exploration_tool.py
@@ -69,6 +69,7 @@ def patch_layernorm_with_rescaled_by_name(model, alpha=1.0, mode="add", match_ke
 	for name, module in model.named_modules():
 		# if isinstance(module, nn.LayerNorm) and any(k in name.lower() for k in match_keywords):
 		pdb.set_trace()
+		print(name)
 		if match_key in name:
 			# Identify the parent module
 			path_parts = name.split(".")
