6c986e0a0a5bd198d748b075a28e90dd38e4f4ba
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-03-16 16:22:46 +0000

    boyan_mac

--- bash_script/desk_script.sh
+++ bash_script/desk_script.sh
@@ -24,7 +24,7 @@ for ((idx=0; idx<${#Target_Para_List[@]}; idx++)); do
 Param=${Target_Para_List[$idx]}
 Tunning=${Tunning_list[$idx]}
 
-sessionname=inputlayernorm_sgd
+sessionname=I2CL_contextlearning_noCE
 File=bash_script/bash_sub_script/sub_layernorm_adaptation.sh
 
 CHECK=${sessionname}
