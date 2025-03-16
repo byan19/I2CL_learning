4d1b685d0676078ab4336652731a8f55342742e8
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-03-16 17:44:56 +0000

    boyan_mac

--- configs/config_layernorm_adaption.py
+++ configs/config_layernorm_adaption.py
@@ -7,7 +7,7 @@ config = {}
 # general
 #config['exp_name'] = 'exps/Llama3_32layers_debug'
 config['exp_name'] = 'exps_restart/Llama2_32layers_conver_bound00000132layer'
-config['gpus'] = ['0']
+config['gpus'] = ['0,1']
 config['models'] = ['meta-llama/Llama-2-7b-hf'] # 'meta-llama/Meta-Llama-3-8B', 'gpt2-xl', 'meta-llama/Llama-2-7b-hf', 'EleutherAI/gpt-j-6B'
 config['datasets'] = list(md.target_datasets.keys())
 config['seed'] = 42
