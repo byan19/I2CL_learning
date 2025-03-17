b2a5b74484b6823e3b5e555b5f0e6eecfadd4b6d
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-03-17 16:22:32 +0000

    boyan_mac

--- bash_script/ibme_script.sh
+++ bash_script/ibme_script.sh
@@ -30,7 +30,7 @@ Param=${Target_Para_List[$idx]}
 Tunning=${Tunning_list[$idx]}
 
 sessionname=I2CL_contextlearning_dyt_learning
-sessionname=I2CL_contextlearning_layernorm_mulfix
+sessionname=I2CL_contextlearning_layernorm_addfix
 File=bash_script/bash_sub_script/sub_layernorm_adaptation.sh
 
 MultiRun ${sessionname} ${File} ${Param} ${Tunning}
--- configs/config_layernorm_adaption.py
+++ configs/config_layernorm_adaption.py
@@ -6,7 +6,7 @@ import my_datasets as md
 config = {}
 # general
 #config['exp_name'] = 'exps/Llama3_32layers_debug'
-config['exp_name'] = 'exps_restart/Llama2_32layers_layernorm_mulfix'
+config['exp_name'] = 'exps_restart/Llama2_32layers_layernorm_addfix'
 config['gpus'] = ['0']
 config['models'] = ['meta-llama/Llama-2-7b-hf'] # 'meta-llama/Meta-Llama-3-8B', 'gpt2-xl', 'meta-llama/Llama-2-7b-hf', 'EleutherAI/gpt-j-6B'
 config['datasets'] = list(md.target_datasets.keys())
@@ -46,5 +46,5 @@ config['post_attention'] = False
 config['input_attention'] = True
 config['conver_bound'] = True
 config['conver_loss_lambda'] = 1e-6
-config['additional_layernorm_mode'] = 'mul'
+config['additional_layernorm_mode'] = 'add'
 config['ce_loss_lambda'] = 1.0
