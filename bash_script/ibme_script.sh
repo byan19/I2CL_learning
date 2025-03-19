bd367f636116962343f1cf6d9481363912f11036
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-03-19 15:12:46 +0000

    boyan_mac

--- bash_script/ibme_script.sh
+++ bash_script/ibme_script.sh
@@ -30,7 +30,7 @@ Param=${Target_Para_List[$idx]}
 Tunning=${Tunning_list[$idx]}
 
 sessionname=I2CL_contextlearning_dyt_learning
-sessionname=I2CL_contextlearning_layernorm_AdamWbatch1
+sessionname=I2CL_contextlearning_layernorm_regular_convloss
 File=bash_script/bash_sub_script/sub_layernorm_adaptation.sh
 
 MultiRun ${sessionname} ${File} ${Param} ${Tunning}
