d0c47d2afe5a442a105bf69047aa91c49b79ac66
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-04-09 15:39:58 +0100

    boyan_mac

--- bash_script/yibo_script.sh
+++ bash_script/yibo_script.sh
@@ -9,8 +9,8 @@ echo "${sessionname}"
 screen -dmS $sessionname
 
 screen -S $sessionname -X stuff "source activate /ibex/user/yangy0o/by_g/by_g_env/i2cl_env^M"
-screen -S $sessionname -X stuff "export HF_HOME=/ibex/user/yangy0o/by_g/huggingface_cache^M"
-screen -S $sessionname -X stuff "export HF_HUB_CACHE=/ibex/user/yangy0o/by_g/huggingface_cache^M"
+#screen -S $sessionname -X stuff "export HF_HOME=/ibex/user/yangy0o/by_g/huggingface_cache^M"
+#screen -S $sessionname -X stuff "export HF_HUB_CACHE=/ibex/user/yangy0o/by_g/huggingface_cache^M"
 
 screen -S $sessionname -X stuff "cd /ibex/user/yangy0o/by_g/I2CL_learning^M"
 sleep 1.0s
@@ -29,7 +29,7 @@ for ((idx=0; idx<${#Target_Para_List[@]}; idx++)); do
 Param=${Target_Para_List[$idx]}
 Tunning=${Tunning_list[$idx]}
 
-sessionname=adding_demonstration_convergenceloss
+sessionname=adding_demonstration_convergenceloss_test
 File=bash_script/bash_sub_script/sub_layernorm_adaptation.sh
 
 CHECK=${sessionname}
