c421dc707ce351c5b77d0aa2e0c7c77ab8181a0b
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-04-26 11:56:39 +0100

    boyan_mac

--- bash_script/desk_script.sh
+++ bash_script/desk_script.sh
@@ -33,6 +33,7 @@ sessionname=I2CL_allLN_sharpnessencoding_alone_noiseinner
 sessionname=version4_1sample_1demon_epoch60_flatMinueconvR
 sessionname=version4_evaluation
 sessionname=version4_desktop_debug
+sessionname=gptdebugging
 
 File=bash_script/bash_sub_script/sub_layernorm_adaptation.sh
 
