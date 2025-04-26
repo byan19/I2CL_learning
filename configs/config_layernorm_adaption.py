2d706922feae8c6a019d852eb2e7b4ceeae7f6ca
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-04-26 11:55:45 +0100

    boyan_mac

--- configs/config_layernorm_adaption.py
+++ configs/config_layernorm_adaption.py
@@ -11,13 +11,14 @@ config['exp_name'] = 'exps_apr7/debugging_with_adding_demonstration'
 config['exp_name'] = 'exps_apr10/version4_1sample_1demon_epoch60_flatMinueconvR_demon3'
 config['exp_name'] = 'exps_apr24/version4_checking_loss_debugging'
 config['exp_name'] = 'exps_apr25/version4_fixedflatness_1sample_1demon_llama3-8b'
-config['exp_name'] = 'exps_apr26/probe_raw'
+config['exp_name'] = 'exps_apr26/debugging_gpt'
 
 config['gpus'] = ['0']
 
 config['models'] = ['EleutherAI/gpt-j-6b'] # 'meta-llama/Meta-Llama-3-8B', 'openai-community/gpt2-xl', 'meta-llama/Llama-2-7b-hf', 'EleutherAI/gpt-j-6B'
 config['models'] = ['meta-llama/Llama-3.1-8B'] # 'meta-llama/Meta-Llama-3-8B', 'openai-community/gpt2-xl', 'meta-llama/Llama-2-7b-hf', 'EleutherAI/gpt-j-6B'
 config['models'] = ['meta-llama/Llama-2-7b-hf'] # 'meta-llama/Meta-Llama-3-8B', 'openai-community/gpt2-xl', 'meta-llama/Llama-2-7b-hf', 'EleutherAI/gpt-j-6B'
+config['models'] = ['EleutherAI/gpt2-xl'] # 'meta-llama/Meta-Llama-3-8B', 'openai-community/gpt2-xl', 'meta-llama/Llama-2-7b-hf', 'EleutherAI/gpt-j-6B'
 
 
 config['datasets'] = list(md.target_datasets.keys())
