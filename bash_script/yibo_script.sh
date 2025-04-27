f77d590c43d20b201010a9d0275f083ddeb72d77
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-04-27 13:37:11 +0100

    boyan_mac

--- bash_script/yibo_script.sh
+++ bash_script/yibo_script.sh
@@ -40,6 +40,7 @@ sessionname=flatMinueconvR_demon1_llama3_8b
 sessionname=probe_ours
 sessionname=llama3_ourmethod
 sessionname=probe_raw3
+sessionname=llama3_again
 File=bash_script/bash_sub_script/sub_layernorm_adaptation.sh
 
 CHECK=${sessionname}
