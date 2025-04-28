f15e1857c55a7de69323b6d9b8653d163e329a61
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-04-28 14:48:37 +0100

    boyan_mac

--- wrapper.py
+++ wrapper.py
@@ -1020,8 +1020,10 @@ class ModelWrapper(nn.Module):
         # cross entropy tuning
         '''
         print('Version 4 based LoRA')
-        pt_config = LoraConfig(r =config['lora_rank'], lora_alpha = 16, target_modules= ['q_proj', 'v_proj', 'k_proj'], lora_dropout=  0.05, bias = "none" , task_type="CAUSAL_LM" )
+        
+        pt_config = LoraConfig(r =config['lora_rank'], lora_alpha = 16, target_modules= ['q_proj', 'v_proj'], lora_dropout=  0.05, bias = "none" , task_type="CAUSAL_LM" )
         #pt_config = LoraConfig(r =128, lora_alpha = 16, target_modules= ['q_proj', 'v_proj', 'k_proj'], lora_dropout=  0.05, bias = "none" , task_type="CAUSAL_LM" )
+        pt_config = LoraConfig(r =config['lora_rank'], lora_alpha = 16, target_modules= ["q_proj", "v_proj", "k_proj", "o_proj", "gate_proj", "up_proj", "down_proj"], lora_dropout=  0.05, bias = "none" , task_type="CAUSAL_LM" )
 
         peft_model = get_peft_model(self.model, pt_config)
         pdb.set_trace()
