d724488b6428ed94b928b7cd490ab3e01e7b3b91
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-04-28 13:41:13 +0100

    boyan_mac

--- my_datasets/__init__.py
+++ my_datasets/__init__.py
@@ -10,7 +10,6 @@ from .rotten_tomatoes import RottenTomatoes
 from .hate_speech18 import HateSpeech18
 from .emo import EMO
 
-'''
 target_datasets = {
     'agnews': AGNews,
     'dbpedia': DBPedia,
@@ -22,8 +21,8 @@ target_datasets = {
     'hate_speech18': HateSpeech18,
     'emo': EMO,
 }
-'''
 
+'''
 # for gpt2-xl
 target_datasets = {
     'sst5': SST5,
@@ -34,6 +33,7 @@ target_datasets = {
     'hate_speech18': HateSpeech18,
     'emo': EMO,
 }
+'''
 
 '''
 target_datasets = {
