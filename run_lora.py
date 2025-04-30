48efc46997c196ca2c76fb956cafcefe28d97c60
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-04-30 19:38:23 +0100

    boyan_mac

--- run_lora.py
+++ run_lora.py
@@ -39,6 +39,7 @@ def main(args):
     # set global seed
     utils.set_seed(args.config['seed'])
     # set device
+    
     args.device = utils.set_device(args.gpu)
     # set comprare metric
     args.metric = args.config['metric']
@@ -148,7 +149,6 @@ def main(args):
         elif args.config['learning_type'] == 'version4':
             model_wrapper.layernorm_adaptation_verion4(args.config, cali_dataset, save_dir=args.save_dir, run_name=run_name)
         elif args.config['learning_type'] == 'rola_analysis':
-            pdb.set_trace()
             model_wrapper.layernorm_adaptation_verion4_basedrola(args.config, cali_dataset, save_dir=args.save_dir,
                                                        run_name=run_name)
         else:
