344786d059fb1e80d9d2e5ee5f1a9efad7a79ae2
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-05-02 14:49:50 +0100

    boyan_mac

--- configs/config_soft_prompt.py
+++ configs/config_soft_prompt.py
@@ -5,7 +5,7 @@ import my_datasets as md
 
 config = {}
 # general
-config['exp_name'] = 'exps_May2_yibo/soft_prompt'
+config['exp_name'] = 'exps_May2_yibo/soft_prompt_llama3'
 config['gpus'] = ['0']
 
 config['models'] = ['meta-llama/Llama-2-7b-hf'] # 'gpt2-xl', 'meta-llama/Llama-2-7b-hf', 'EleutherAI/gpt-j-6B'
