d6278c7b2bacdfa3339a3a301fd9a73a0bea1841
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-05-02 17:48:24 +0100

    boyan_mac

--- configs/config_label_anchor.py
+++ configs/config_label_anchor.py
@@ -5,13 +5,17 @@ import my_datasets as md
 
 config = {}
 # general
-config['exp_name'] = 'exps/label_anchor'
+config['exp_name'] = 'exps_May2_desk/label_anchor_llama3_instruct'
+
 config['gpus'] = ['0']
 config['models'] = ['meta-llama/Llama-2-7b-hf'] # 'gpt2-xl', 'meta-llama/Llama-2-7b-hf', 'EleutherAI/gpt-j-6B'
+config['models'] = ['meta-llama/Meta-Llama-3-8B'] # 'meta-llama/Meta-Llama-3-8B', 'openai-community/gpt2-xl', 'meta-llama/Llama-2-7b-hf', 'EleutherAI/gpt-j-6B'
+config['models'] = ['meta-llama/Meta-Llama-3-8B-Instruct'] # 'meta-llama/Meta-Llama-3-8B', 'openai-community/gpt2-xl', 'meta-llama/Llama-2-7b-hf', 'EleutherAI/gpt-j-6B'
+
 config['datasets'] = list(md.dataset_classification.keys())
 config['seed'] = 42
 config['run_num'] = 5
-config['run_baseline'] = True
+config['run_baseline'] = False
 config['metric'] = 'acc'  # 'acc', 'macro_f1'
 config['bs'] = 2
 config['load_in_8bit'] = False
--- run_label_anchor.py
+++ run_label_anchor.py
@@ -239,6 +239,12 @@ def main(args):
         with open(args.save_dir + '/result_dict.json', 'w') as f:
             json.dump(result_dict, f, indent=4)
     
+    if config['run_baseline']:
+        utils.result_mean_calculator(result_dict, 'few_shot')
+    utils.result_mean_calculator(result_dict, 'ours')
+    # save result_dict after each run
+    with open(args.save_dir + '/result_dict.json', 'w') as f:
+        json.dump(result_dict, f, indent=4)
     # delete all variables
     del model_wrapper, model, tokenizer, train_dataset, test_dataset
     del test_evaluator
@@ -284,11 +290,4 @@ if __name__ == "__main__":
                 time.sleep(5)
 
     # Create a process for each GPU
-    processes = [Process(target=run_task, args=(gpu_id, config)) for gpu_id in config['gpus']]
-    # Start all processes
-    for p in processes:
-        p.start()
-    # Wait for all processes to finish
-    for p in processes:
-        p.join()
-    print("All tasks completed.")
+    run_task('0', config)  # without parallelisation
