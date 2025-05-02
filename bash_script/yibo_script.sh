b83125aead05269e7287cbe3d99b3cf041c1e34f
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-05-02 14:49:04 +0100

    boyan_mac

--- bash_script/yibo_script.sh
+++ bash_script/yibo_script.sh
@@ -41,9 +41,11 @@ sessionname=llama3_ourmethod_apr29
 sessionname=gpt_j_6b_version4_apr30
 sessionname=llama3instruct_ce_zerofewshot
 sessionname=llama3_ce_zero_fewshot
+sessionname=softprompt_llama3
 
 File=bash_script/bash_sub_script/sub_lora.sh
 File=bash_script/bash_sub_script/sub_layernorm_adaptation.sh
+File=bash_script/bash_sub_script/sub_run.sh
 
 CHECK=${sessionname}
 
