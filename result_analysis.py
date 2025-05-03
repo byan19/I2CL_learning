cb681dadd3f5df526fdf82903c39226237725181
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-05-03 18:00:29 +0100

    boyan_mac

--- result_analysis.py
+++ result_analysis.py
@@ -14,6 +14,10 @@ def analysis(exp_name, model_name, dataset_list):
 		file_name = f'exps_apr29/llama3_8B_version4/meta-llama/Meta-Llama-3-8B/{dataset}/result_dict.json'
 		file_name = f'exps_May2_yibo/soft_prompt_llama3_instruct/meta-llama/Meta-Llama-3-8B-Instruct/{dataset}/result_dict.json'
 		file_name = f'exps_May2_yibo/soft_prompt_llama3/meta-llama/Meta-Llama-3-8B/{dataset}/result_dict.json'
+		file_name = f'exps_May2_yibo/label_anchor_llama3_instruct/meta-llama/Meta-Llama-3-8B/{dataset}/result_dict.json'
+		
+		file_name = f'exps_May2_yibo/task_vector_llama3/meta-llama/Meta-Llama-3-8B/{dataset}/result_dict.json'
+		file_name = f'exps_May2_yibo/task_vector_llama3_instruct/meta-llama/Meta-Llama-3-8B/{dataset}/result_dict.json'
 
 
 		print(file_name)
