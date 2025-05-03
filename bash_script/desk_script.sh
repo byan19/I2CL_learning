a616dd34f0ce29397b323e34164ee3c9a9bbb4f9
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-05-03 18:31:20 +0100

    boyan_mac

--- bash_script/desk_script.sh
+++ bash_script/desk_script.sh
@@ -33,6 +33,7 @@ sessionname=lora16_scripttest
 sessionname=softprompt
 sessionname=taskvector
 sessionname=label_ancho
+sessionname=test_i2cl
 
 File=bash_script/bash_sub_script/sub_layernorm_adaptation.sh
 File=bash_script/bash_sub_script/sub_lora.sh
