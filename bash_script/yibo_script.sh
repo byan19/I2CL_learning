d5de013bca44f511570d69ec9ce8dd92c03bc3f0
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-05-08 21:00:04 +0100

    boyan_mac

--- bash_script/yibo_script.sh
+++ bash_script/yibo_script.sh
@@ -47,7 +47,7 @@ sessionname=i2cl_llama3_instruct
 sessionname=label_anchor_llama3
 sessionname=lora16_llama2
 sessionname=lora1all_llama2_ours
-sessionname=ia3_llama2
+sessionname=ia3_llama3_instruct
 
 File=bash_script/bash_sub_script/sub_layernorm_adaptation.sh
 File=bash_script/bash_sub_script/sub_lora.sh
