3464fac75f6136be66c7623541284e3cba1ea3db
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-05-08 20:58:28 +0100

    boyan_mac

--- bash_script/yibo_script.sh
+++ bash_script/yibo_script.sh
@@ -47,10 +47,11 @@ sessionname=i2cl_llama3_instruct
 sessionname=label_anchor_llama3
 sessionname=lora16_llama2
 sessionname=lora1all_llama2_ours
+sessionname=ia3_llama2
 
 File=bash_script/bash_sub_script/sub_layernorm_adaptation.sh
-File=bash_script/bash_sub_script/sub_run.sh
 File=bash_script/bash_sub_script/sub_lora.sh
+File=bash_script/bash_sub_script/sub_run.sh
 
 CHECK=${sessionname}
 
